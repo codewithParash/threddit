29f4c2d7ce6fbeab1516fdd297f0b842077e882c
ecobeesparash <parash.shahi@ecobees.net>
2023-12-03 13:29:59 +0545

    feat: user menu.

--- components/common/NavBar.tsx
+++ components/common/NavBar.tsx
@@ -1,22 +1,9 @@
-// "use client";
 import React from "react";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
 
-import ToggleTheme from "@/components/common/ToggleTheme";
-import Container from "@/components/common/Container";
 import DialogModal from "@/components/common/DialogModal";
-import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
+import OAuthLogin from "@/components/common/OAuthLogin";
+import UserMenu from "@/components/UserMenu";
 import { getAuthSession } from "@/lib/auth";
-import OAuthLogin from "./OAuthLogin";
 
 const components: { title: string; href: string; description: string }[] = [
   {
@@ -60,79 +47,17 @@ const NavBar = async () => {
   const session = await getAuthSession();
 
   return (
-    <div className="flex py-6 justify-between">
-      <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                  <li className="row-span-3">
-                    {/* <NavigationMenuLink asChild>
-                      <a
-                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                        href="/"
-                      >
-                        <div className="mb-2 mt-4 text-lg font-medium">
-                          shadcn/ui
-                        </div>
-                        <p className="text-sm leading-tight text-muted-foreground">
-                          Beautifully designed components built with Radix UI
-                          and Tailwind CSS.
-                        </p>
-                      </a>
-                    </NavigationMenuLink> */}
-                  </li>
-                  {/* <ListItem href="/docs" title="Introduction">
-                    Re-usable components built using Radix UI and Tailwind CSS.
-                  </ListItem>
-                  <ListItem href="/docs/installation" title="Installation">
-                    How to install dependencies and structure your app.
-                  </ListItem>
-                  <ListItem
-                    href="/docs/primitives/typography"
-                    title="Typography"
-                  >
-                    Styles for headings, paragraphs, lists...etc
-                  </ListItem> */}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                  {/* {components.map((component) => (
-                    <ListItem
-                      key={component.title}
-                      title={component.title}
-                      href={component.href}
-                    >
-                      {component.description}
-                    </ListItem>
-                  ))} */}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/docs" legacyBehavior passHref>
-                {/* <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  Documentation
-                </NavigationMenuLink> */}
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+    <div className="flex py-6 justify-between items-center">
+      <p>Threddit</p>
 
       <div className="flex items-center gap-4">
         {/* <ToggleTheme /> */}
         {session?.user ? (
-          <span>Logged In</span>
+          <UserMenu user={session.user} />
         ) : (
-          // <DialogModal buttonText="Sign in">
-          //   <OAuthLogin />
-          // </DialogModal>
-          <span>Login</span>
+          <DialogModal buttonText="Sign in">
+            <OAuthLogin />
+          </DialogModal>
         )}
       </div>
     </div>
@@ -140,29 +65,3 @@ const NavBar = async () => {
 };
 
 export default NavBar;
-
-// const ListItem = React.forwardRef<
-//   React.ElementRef<"a">,
-//   React.ComponentPropsWithoutRef<"a">
-// >(({ className, title, children, ...props }, ref) => {
-//   return (
-//     <li>
-//       <NavigationMenuLink asChild>
-//         <a
-//           ref={ref}
-//           className={cn(
-//             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-//             className
-//           )}
-//           {...props}
-//         >
-//           <div className="text-sm font-medium leading-none">{title}</div>
-//           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-//             {children}
-//           </p>
-//         </a>
-//       </NavigationMenuLink>
-//     </li>
-//   );
-// });
-// ListItem.displayName = "ListItem";
